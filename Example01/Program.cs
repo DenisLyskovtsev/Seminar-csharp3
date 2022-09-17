ce19c2b92f8977cc3fb91e4608b73d9a409c55bc
Denis <d.lyskovtsev@mail.ru>
2022-09-17 15:06:16 +0400

    Homework1

---
+++ Example01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ведите пятизначное целое число");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 10 == number / 10000 % 10 && number / 1000 % 10 == number / 10 % 10)
+{
+    Console.WriteLine("Даное число является палиндромом");
+}
+else
+{
+    Console.WriteLine("Даное число не является палиндромом");
+}
